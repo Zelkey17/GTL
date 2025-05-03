a4116ed023d0ce0f147c2d517d1900075648b479
Иван Денисович Фомин <71864626+Zelkey17@users.noreply.github.com>
2025-05-04 01:59:02 +0300

    Update permutation_element.py

--- elements/permutation_element.py
+++ elements/permutation_element.py
@@ -33,7 +33,7 @@ class PermutationElement(Element[list]):
     def __repr__(self) -> str:
         return str(*self._value)
 
-    def cyclic_presentation(self, is_add_trivial : bool) -> list:
+    def cyclic_presentation(self, include_trivial : bool) -> list:
         perm = []
         used = [range(1, len(self) + 1)]
 
@@ -42,7 +42,7 @@ class PermutationElement(Element[list]):
                 cyc = (self._value[i], )
                 while (self._value[cyc[-1] - 1] != cyc[0]):
                     cyc += (self._value[cyc[-1] - 1], )
-                if len(cyc) > 1 or is_add_trivial:
+                if len(cyc) > 1 or include_trivial:
                     perm += [cyc]
         return perm
 
