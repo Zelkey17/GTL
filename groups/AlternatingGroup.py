dedaa2eaafa70b218d963a31e9ef84bc2c226948
Yaroslav-7 <shul.yaroslav7@gmail.com>
2025-05-19 05:11:56 +0300

    TOTOTOTOOTOTOTOTOTOTOTo

--- groups/AlternatingGroup.py
+++ groups/AlternatingGroup.py
@@ -147,7 +147,7 @@ class AlternatingGroup(FiniteGroup[List[int], PermutationElement]):
         Коммутант (производная группа) A_n.
 
         Для n ≤ 3 A_n абелева ⇒ коммутант тривиален.
-        Для n > 4 A_n проста ⇒ коммутант = сама группа.
+        Для n ≥ 4 A_n проста ⇒ коммутант = сама группа.
 
         Returns:
             SubGroup[PermutationElement]: Коммутант A_n.
